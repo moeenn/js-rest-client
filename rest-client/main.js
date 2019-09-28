f9e1dec696706d81318b2bf891b3b42ecabfdaaa
moeenn <moeen.v8@gmail.com>
2019-09-29 01:01:27 +0500

    implemented GET and DELETE

--- rest-client/main.js
+++ rest-client/main.js
@@ -1,9 +1,33 @@
-// request object
-class Request {
-	constructor( url, method ) {
-		this.url = url;
-		this.method = method;
-		this.header = "Content-Type: application/json";
-		this.body = "";
+// UI class
+class UI {
+	static WriteResponse(response) {
+		const respnseArea = document.querySelector('#response_area');
+		respnseArea.innerHTML = JSON.stringify(response, null, 2);
 	}
 }
+
+// get the JSON response from REST API
+function getResponse( method, url ) {
+	// create request object
+	const request = new XMLHttpRequest();
+	request.open(method, url, true);
+
+	// perform action on json response
+	request.onload = function() {
+		const receivedData = JSON.parse(this.response);
+		console.log(receivedData);
+		UI.WriteResponse(receivedData);
+	}
+	request.send();
+}
+
+// event
+document.querySelector('#entry_form').addEventListener('submit', (event) => {
+	// prevent submission
+	event.preventDefault();
+
+	const method = document.querySelector('#request_method').value;
+	const url = document.querySelector('#address').value;
+
+	getResponse(method, url);
+});
