1ef8ba157a15d6cf550278c2d92127d8ffa641b9
moeenn <moeen.v8@gmail.com>
2019-10-02 22:41:27 +0500

    finalized the UI and introduced animations

--- rest-client/main.js
+++ rest-client/main.js
@@ -1,33 +1,54 @@
-// UI class
-class UI {
-	static WriteResponse(response) {
-		const respnseArea = document.querySelector('#response_area');
-		respnseArea.innerHTML = JSON.stringify(response, null, 2);
-	}
-}
+// globals
+let METHOD;
 
-// get the JSON response from REST API
-function getResponse( method, url ) {
-	// create request object
-	const request = new XMLHttpRequest();
-	request.open(method, url, true);
-
-	// perform action on json response
-	request.onload = function() {
-		const receivedData = JSON.parse(this.response);
-		console.log(receivedData);
-		UI.WriteResponse(receivedData);
-	}
-	request.send();
-}
-
-// event
-document.querySelector('#entry_form').addEventListener('submit', (event) => {
+// watch for submit
+document.querySelector('#entry-form').addEventListener('submit', (event) => {
 	// prevent submission
 	event.preventDefault();
 
-	const method = document.querySelector('#request_method').value;
-	const url = document.querySelector('#address').value;
+	// get the form values
+	const URL = document.querySelector('#address').value;
 
-	getResponse(method, url);
+	// get optional fields
+	// if (METHOD == 'POST' || METHOD == 'PUT') {}
+
+	const xhr = new XMLHttpRequest();
+	xhr.open(METHOD, URL, true);
+
+	// loading
+	xhr.onprogress = function() {
+		console.log('Sending Request ...');
+	}
+
+	// on completion
+	xhr.onload = function() {
+		if (this.status == 200) {
+			console.log('Response Status: OK');
+			console.log(this.responseText);
+		} else if ( this.status == 404 ) {
+			console.log('Response Status: Not Found (404)');
+		}
+	}
+
+	// report error on response
+	xhr.onerror = function() {
+		console.log('Failed to receive response from server!');
+	}
+
+	// send resquest
+	xhr.send();
 });
+
+// menu item selection
+document.querySelector('#request-method').addEventListener('input', GetMethod);
+
+function GetMethod() {
+	METHOD = document.querySelector('#request-method').value;
+	const optionalFields = document.querySelector('#optional-fields')
+
+	if (METHOD == 'POST' || METHOD == 'PUT') {
+		optionalFields.style.display = 'block';
+	} else {
+		optionalFields.style.display = 'none';
+	}
+}
