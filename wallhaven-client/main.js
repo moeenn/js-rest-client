e263d962a610f33cea8e41dceba1c9edcf7eba17
moeenn <moeen.v8@gmail.com>
2019-10-03 00:07:20 +0500

    a client for wallhaven wallpapers website

---
+++ wallhaven-client/main.js
@@ -0,0 +1,69 @@
+// globals
+const apiKey = '1s9mBrXtijsRplYRXqJkhOkMdUdAWl67';
+const purity = 111; // 100 SFW, 110 Sketcy, 111 NSFW
+let pageNum = 1;
+
+
+// wallpaper class
+class Wallpaper {
+	constructor(path, thumb) {
+		this.path = path;
+		this.thumb = thumb;
+	}
+}
+
+// UI class
+class UI {
+	static AddWallToDoc(wallpaper) {
+		const wallArea = document.querySelector('#wallpapers');
+		const wallGallery = document.createElement('div');
+		wallGallery.className = 'item shadow-large'
+		wallGallery.innerHTML = `
+		<a href=${wallpaper.path} target="_blank">
+			<img src=${wallpaper.thumb}  alt="wallpaper"/>
+		</a>`
+
+		wallArea.appendChild(wallGallery);
+		console.log('Added Wallpaper to Document:', wallpaper);
+	}
+}
+
+// event listener
+document.addEventListener('DOMContentLoaded', GetWallpapers);
+
+function GetWallpapers() {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', `https://wallhaven.cc/api/v1/search?apikey=${apiKey}&page=${pageNum}&purity=${purity}`, true);
+
+	// loading
+	xhr.onprogress = function() {
+		console.log('Connecting to Wallhaven API ...');
+	}
+
+	// on load
+	xhr.onload = function() {
+		console.log('Request Completed!');
+
+		// check status
+		if (this.status == 200) {
+			const data = JSON.parse(this.responseText);
+			const wallpapers = data.data;
+
+			for ( let wallpaper of wallpapers) {
+				const wall = new Wallpaper( wallpaper.path, wallpaper.thumbs.small );
+				UI.AddWallToDoc(wall);
+			}
+
+		} else if (this.status == 404) {
+			console.log('Failed to Connect: Not Found');
+		}
+	}
+
+	// error handling
+	xhr.onerror = function() {
+		console.log('Failed to Connect to API!');
+	}
+
+	// send request
+	xhr.send();
+}
